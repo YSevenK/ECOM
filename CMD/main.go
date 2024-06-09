50ccf947b9d9195edecb4e4b05d48f465ed7ff19
ysevenk <3275223747@qq.com>
2024-06-09 17:26:09 +0800

    user service

--- CMD/main.go
+++ CMD/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/YSevenK/ecom/CMD/api"
+)
+
 func main() {
-    
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
