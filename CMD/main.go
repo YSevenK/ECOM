1d17f6e8622237809068195d732c0f78bb19d70a
ysevenk <3275223747@qq.com>
2024-06-09 18:02:45 +0800

    DataBase Connect

--- CMD/main.go
+++ CMD/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/YSevenK/ecom/CMD/api"
+	"github.com/YSevenK/ecom/config"
+	"github.com/YSevenK/ecom/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err) // 处理数据库连接错误
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to database")
 }
